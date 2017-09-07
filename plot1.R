aac8ec6e09a20dd5f4518a4b8d27e0bce3747788
gmartinon <gregoire.martinon@gmail.com>
2017-09-07 10:38:05 +0200

    R files better formatted

--- plot1.R
+++ plot1.R
@@ -6,7 +6,7 @@ df <- read.csv("household_power_consumption.txt",
                sep = ";", na.strings = "?", nrows = 70000)
 
 # select only the dates between February, 1st and February 2nd, 2007
-df$Date <- dmy(df$Date)
+df <- df %>% mutate(Date = dmy(Date))
 df <- df %>% filter(Date %in% ymd("2007-02-01"):ymd("2007-02-02"))
 
 # plot the histogram
--- plot3.R
+++ plot3.R
@@ -6,11 +6,11 @@ df <- read.csv("household_power_consumption.txt",
                sep = ";", na.strings = "?", nrows = 70000)
 
 # select only the dates between February, 1st and February 2nd, 2007
-df$Date <- dmy(df$Date)
+df <- df %>% mutate(Date = dmy(Date))
 df <- df %>% filter(Date %in% ymd("2007-02-01"):ymd("2007-02-02"))
 
 # create datetime object
-df$Time <- hms(df$Time)
+df <- df %>% mutate(Time = hms(Time))
 datetime <- df$Date + df$Time
 
 # set the locale to english for plots ticks (otherwise it is in french)
